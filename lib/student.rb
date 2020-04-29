5fc8f9d9f926caea6916275a883aa06d8b4cb8fb
aidanmc95 <aidanmc95@gmail.com>
2020-04-28 17:11:01 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,53 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+ 
+    set = DB[:conn].execute(sql)
+    set.map {|student| self.new_from_db(student)}
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    self.all.find {|student| student.name == name}
+  end
+
+  def self.all_students_in_grade_9
+    self.all.select {|student| student.grade == 9}
+  end
+
+  def self.students_below_12th_grade
+    self.all.select {|student| student.grade < 12}
+  end
+
+  def self.first_X_students_in_grade_10(limit)
+    self.all.select {|student| student.grade == 10}.first(limit)
+  end
+
+  def self.first_student_in_grade_10
+    self.all.select {|student| student.grade == 10}.first
+  end
+
+  def self.all_students_in_grade_X(current_grade)
+    self.all.select {|student| student.grade == current_grade}
   end
   
   def save
@@ -29,7 +64,7 @@ class Student
     CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
-      grade TEXT
+      grade INTEGER
     )
     SQL
 
